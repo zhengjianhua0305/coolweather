07aa5be4160fc022768af728037f91204fdb0dc5
zhengjianhua0305 <1070481649@qq.com>
2020-08-20 23:30:16 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/coolweather/fragment/ChooseAreaFragment.java
@@ -0,0 +1,8 @@
+package com.example.coolweather.fragment;
+
+/**
+ * Create by ${郑建华}
+ * on 2020年8月20日20:58:10
+ */
+public class ChooseAreaFragment {
+}
---
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -0,0 +1,8 @@
+package com.example.coolweather.util;
+
+/**
+ * Create by ${郑建华}
+ * on 2020年8月20日20:37:28
+ */
+public class Utility {
+}
